149b1fa6ebc6819b978b947b372ded14c830ac3c
lottoian <32203267@dankook.ac.kr>
2021-06-17 14:03:48 +0900

    *-*

--- CH01/indeed.py
+++ CH01/indeed.py
@@ -19,7 +19,16 @@ def extract_indeed_pages():
     return max_page
 
 def extract_indeed_jobs(last_page):
-    for page in range(last_page):
-        result = requests.get(f"{url}&start={page*limit}")
-        print(result.status_code)
-    
+    #for page in range(last_page):
+    result = requests.get(f"{url}&start={0*limit}")
+    soup = BeautifulSoup(result.text,"html.parser")
+    results = soup.find_all("div",{"class":"job_seen_beacon"})
+      
+    for text in results:
+        title = text.find("h2",{"class":"jobTitle jobTitle-color-purple"}).find("span")["title"]
+        if title is not None:
+            print(title)
+        
+max_indeed_pages = extract_indeed_pages()
+print(max_indeed_pages)
+extract_indeed_jobs(int(max_indeed_pages))
